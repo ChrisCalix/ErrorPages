9d2828a2f5d7b3ace085179f9c0e4e7e9fd4bc75
christian.sonic <yoh.chrisitan@gmail.com>
2023-02-24 17:30:43 -0400

    Initial Commit

---
+++ ErrorPages/ErrorPagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorPagesApp.swift
+//  ErrorPages
+//
+//  Created by Sonic on 24/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ErrorPagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
