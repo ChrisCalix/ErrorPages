9d544195143ac80eae016b6b0456d59dd5e224ad
christian.sonic <yoh.chrisitan@gmail.com>
2023-02-24 19:44:48 -0400

    add assets for screens and make the first screen error UI

--- ErrorPages/ContentView.swift
+++ ErrorPages/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack(alignment: Alignment(horizontal: .leading, vertical: .bottom)) {
+            Image("1_no_connection")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(alignment: .leading, spacing: 30) {
+                Text("Oops! ")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                
+                Text("Something is wrong with your \n connection, Please try \nagain")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .opacity(0.7)
+                
+                Button {
+                    
+                } label: {
+                    Text("Retry".uppercased())
+                        .fontWeight(.semibold)
+                        .foregroundColor(.black)
+                        .padding(.vertical)
+                        .padding(.horizontal, 30)
+                        .background(Capsule().foregroundColor(.white))
+                }
+
+            }
+            .padding(.horizontal, 70)
+            .padding(.bottom, UIScreen.main.bounds.height * 0.1)
+            
         }
-        .padding()
     }
 }
 
